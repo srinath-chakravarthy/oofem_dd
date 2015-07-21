09759e3dcb890237beb03b0a78e0b861fb0add69
Srinath Chakravarthy <s.chakravarthy@neu.edu>
2015-07-21 16:46:58 -0400

    Removed build files, added to gitignore

--- build/CMakeFiles/liboofem.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "liboofem.pdb"
-  "liboofem.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/liboofem.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
