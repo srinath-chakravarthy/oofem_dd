548c10346ede4723c96f6ece237820522837fa96
Alp <elci.a@husky.neu.edu>
2015-09-03 23:28:05 -0400

    Dirty spawn implementation

--- src/dd/point/source.cpp
+++ src/dd/point/source.cpp
@@ -1,11 +1,12 @@
 #include "point/source.h"
+#include "point/dislocation.h"
 #include "../forcefunctor/fromdislocations.h"
 #include "../forcefunctor/fromfem.h"
-#include <string>
+#include "domain.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 namespace dd {
-
-
     void SourcePoint::setCaches() {
         caches.clear();
         caches.reserve(2);
@@ -13,5 +14,36 @@ namespace dd {
         caches.push_back(ForceCache(FromFem::getInstance()));
     }
     
-    
+    void SourcePoint::spawn(const double & dt, const double & tnuc) {
+    	double ratio = cachedForce().abs() / getBurgersMagnitude();
+    	if(ratio > __strength) {
+    		__timer += dt;
+    	}
+    	
+    	if(__timer > tnuc) {
+    		double nu = getDomain()->getPassionsRatio();
+    		double inuc = getDomain()->getModulus() / (4 * M_PI * (1 - nu * nu) * __strength);
+    		
+    		double posNegative = getSlipPlanePosition() - 0.5 * inuc;
+    		double posPositive = getSlipPlanePosition() + 0.5 * inuc;
+    		
+    		int signNeg = (ratio < 0)? 1 : -1;
+    		int signPos = signNeg * -1;
+    		
+    		std::list<Point *> & dislocs = getSlipPlane()->getContainer("DislocationPoint");
+    		
+    		auto itN = dislocs.begin(), itP = dislocs.begin();
+    		
+    		while(itN != dislocs.end() && (*itN)->getSlipPlanePosition() < posNegative) {
+    			itN++;
+    		}
+    		while(itP != dislocs.end() && (*itP)->getSlipPlanePosition() < posPositive) {
+    			itP++;
+    		}
+    		
+    		new DislocationPoint(getDomain(), getSlipPlane(), itN, posNegative, signNeg);
+    		new DislocationPoint(getDomain(), getSlipPlane(), itP, posPositive, signPos);
+    		
+    	}
+    }
 }
--- src/dd/point/source.h
+++ src/dd/point/source.h
@@ -11,26 +11,36 @@ namespace dd {
 #define SOURCEPOINT_NAME "SourcePoint"
     private:
         double __strength;
+        double __length;
+        double __timer;
         void setCaches();
     public:
 
-        SourcePoint(Domain * domain, SlipPlane * sPlane, double slipPlanePosition, double strength = 0) :
+        SourcePoint(Domain * domain, SlipPlane * sPlane, double slipPlanePosition, double strength = 0, double length = 0,
+        			double timer = 0) :
             Point(slipPlanePosition),
-            __strength(strength) {
+            __strength(strength),
+            __length(length),
+            __timer(timer) {
             setRegistrations(domain, sPlane);
             setCaches();
         }
         SourcePoint(Domain * domain, SlipPlane * sPlane,
-                    list<Point *>::iterator antecedentIt, double slipPlanePosition, double strength = 0) :
+                    list<Point *>::iterator antecedentIt, double slipPlanePosition, double strength = 0, double length = 0,
+                    double timer = 0) :
             Point(slipPlanePosition),
-            __strength(strength) {
+            __strength(strength),
+            __length(length),
+            __timer(timer) {
             setRegistrations(domain, sPlane, antecedentIt);
             setCaches();
         }
         
         double strength() const { return __strength; }
+        double length() const { return __length; }
+        double timer() const { return __timer; }
         
-        
+        void spawn(const double & dt, const double & tnuc);
 
         virtual string typeName() const { return SOURCEPOINT_NAME; }
         static string staticTypeName() { return SOURCEPOINT_NAME; }
